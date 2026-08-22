bda0a6f69b879a5e1dda9b4c86b8d33b275906e8
Emmanuel Adeleke <caesaradek@gmail.com>
2026-08-22 18:31:26 +0000

    added user for code yttest

---
+++ src/main/java/com/example/user/User.java
@@ -0,0 +1,43 @@
+package com.example.user;
+
+import java.util.Objects;
+
+public final class User {
+
+    private final String name;
+    private final int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User other)) {
+            return false;
+        }
+        return age == other.age && Objects.equals(name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', age=" + age + "}";
+    }
+}
